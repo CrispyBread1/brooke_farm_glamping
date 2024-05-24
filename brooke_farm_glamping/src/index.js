431b74e684ad0f5ff0277d0fd7b7e376dce52bcf
Daniel <dsalvatori2008@gmail.com>
2024-05-24 16:55:13 +0100

    playing around with databases

--- brooke_farm_glamping/src/index.js
+++ brooke_farm_glamping/src/index.js
@@ -3,13 +3,40 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, set } from "firebase/database";
+ 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAms2TxN-V_0N0q56ERISmsZnzv5RTdnmY",
+  authDomain: "brooke-farm-glamping.firebaseapp.com",
+  projectId: "brooke-farm-glamping",
+  storageBucket: "brooke-farm-glamping.appspot.com",
+  messagingSenderId: "900497975699",
+  appId: "1:900497975699:web:bb6906451214461bdc2066"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const writeToDB = (userId, userName, userAge) => {
+  
+  const db = getDatabase();
+  const reference = ref(db, 'users/' = userId)
+
+  set(reference, {
+    userID: userId,
+    Name: userName,
+    Age: userAge
+  })
+}
+
+writeToDB("111", "Dan", "24")
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
