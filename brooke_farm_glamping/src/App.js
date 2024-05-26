112b96ab20b99cfaa5e8fee0bcdaaec96f2ce916
Daniel <dsalvatori2008@gmail.com>
2024-05-26 13:09:46 +0100

    added log in page

--- brooke_farm_glamping/src/App.js
+++ brooke_farm_glamping/src/App.js
@@ -2,20 +2,30 @@ import './App.css';
 import React, { useState } from 'react';
 import HomePage from './Containers/homePage';
 import BookingPage from './Containers/bookingPage';
+import LogInPage from './Containers/logInPage';
 
 function App({newBooking}) {
 
   const [home, setHome] = useState(true)
   const [booking, setBooking] = useState(false)
+  const [logIn, setLogIn] = useState(false)
 
   const changeHomePage = () => {
     setHome(true);
-      setBooking(false);
+    setBooking(false);
+    setLogIn(false);
   }
 
   const changeBookingPage = () => {
-      setHome(false);
-      setBooking(true);
+    setHome(false);
+    setBooking(true);
+    setLogIn(false);
+  }
+
+  const changeLogInPage = () => {
+    setHome(false);
+    setBooking(false);
+    setLogIn(true);
   }
 
 
@@ -26,11 +36,13 @@ function App({newBooking}) {
     <div className="NavButtons">
             <div id="Home-Button"  onClick={changeHomePage} value="Home">Home</div>
             <div id="Book-Button" onClick={changeBookingPage} value="Book">Book</div>
+            <div id="LogIn-Button" onClick={changeLogInPage} value="LogIn">Log in</div>
     </div>
 
     <div className="Rendering pages">
       {home && <HomePage />}
       {booking && <BookingPage newBooking={newBooking}/>}
+      {logIn && <LogInPage/>}
     </div>
     </div>
   );
---
+++ brooke_farm_glamping/src/Containers/logInPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LogInPage = () => {
+
+    return (
+        <div>
+            <p>Log in page</p>
+        </div>
+    ) 
+
+}
+
+export default LogInPage;
