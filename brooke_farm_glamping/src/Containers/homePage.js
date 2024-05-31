7aa4e5e711cc2cbe375dcac32b3f7c6b761f21d3
Daniel <dsalvatori2008@gmail.com>
2024-05-31 13:05:31 +0100

    playing with backgrounds

--- brooke_farm_glamping/src/Containers/homePage.js
+++ brooke_farm_glamping/src/Containers/homePage.js
@@ -1,4 +1,7 @@
 import React from "react";
+import './homPage.css'
+import backGround1 from '../JPGs/Background/IMG_2978.JPG'
+import backGround2 from '../JPGs/Background/IMG_3211.JPG'
 
 const HomePage = ({}) => {
     
@@ -7,8 +10,10 @@ const HomePage = ({}) => {
 
     return (
         <div className="Home">
-            <p>home page bro</p>
-        
+            {/* <p>home page bro</p> */}
+            <div className="Background">
+            <img id="book-Image"  src={backGround2}  />
+            </div>
             
     </div>
     )
