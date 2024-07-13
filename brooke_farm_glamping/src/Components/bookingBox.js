0c0ee31aa6fd13f2104658e5f5b2a9ed47159190
Daniel <dsalvatori2008@gmail.com>
2024-07-13 13:50:27 +0100

    booking box, added two sections playing around how it looks and uses

--- brooke_farm_glamping/src/Components/bookingBox.js
+++ brooke_farm_glamping/src/Components/bookingBox.js
@@ -6,14 +6,7 @@ import { useState } from "react";
 
 const BookingBox = ({}) => {
 
-    const [nights, setNights] = useState(1)
-    const [accomodation, setAccomodation] = useState([])
-    const [pitchesAmount, setPitchesAmount] = useState([])
-    const [date, setDate] = useState('') // Maybe? mihght just pass through from calender container
-    const [peopleAmount, setPeopleAmount] = useState(1)
-    const [childrenAmount, setChildrenAmount] = useState(0)
-    const [infantsAmount, setInfantsAmount] = useState(0)
-    const [dogAmount, setDogAmount] = useState(0)
+    
 
     return (
         <>
--- brooke_farm_glamping/src/Containers/bookingContainer.js
+++ brooke_farm_glamping/src/Containers/bookingContainer.js
@@ -5,23 +5,72 @@ import './bookingContainer.css'
 
 const BookingContainer = ({bookingBoxOpen}) => {
 
-    const [openBox, setOpenBox] = useState(bookingBoxOpen);
+    const [booking, setBooking] = useState(bookingBoxOpen)
 
-    useEffect(() => {
+    const [nights, setNights] = useState(1)
+    const [accomodation, setAccomodation] = useState([])
+    const [pitchesAmount, setPitchesAmount] = useState([])
+    const [date, setDate] = useState('') // Maybe? mihght just pass through from calender container
+    const [peopleAmount, setPeopleAmount] = useState(1)
+    const [childrenAmount, setChildrenAmount] = useState(0)
+    const [infantsAmount, setInfantsAmount] = useState(0)
+    const [dogAmount, setDogAmount] = useState(0)
 
-        if (openBox) {
-             openBookingBox()
+    useEffect(() => { // Renders when bookings have come through
+        if (!booking) {
+            clearBookingInformation()
         }
+        console.log(booking)
+    }, [])
 
-    }, [openBox]);
+    const clearBookingInformation = () => {
+        setNights(1)
+        setAccomodation([])
+        setPitchesAmount([])
+        setDate('')
+        setPeopleAmount(1)
+        setChildrenAmount(0)
+        setChildrenAmount(0)
+        setInfantsAmount(0)
+        setDogAmount(0)
+    }
 
-    const openBookingBox = () => {
+    const handleFormSubmit = (evt) => {
+        evt.preventDefault();
+        return;
+    }
 
+    const handleNightsChange = (evt) => {
+        setNights(evt.target.value);
+    }
+
+    const handleAdultsChange = (evt) => {
+        setPeopleAmount(evt.target.value)
     }
 
     return (
         <div id="booking-box">
-            <BookingBox/>
+            {/* <BookingBox/> */}
+            <form onSubmit={handleFormSubmit}>
+                <input
+                type="number"
+                placeholder="number"
+                value={nights}
+                onChange={handleNightsChange}
+                />
+                <input
+                className="adults"
+                type="number"
+                value={peopleAmount}
+                onChange={handleAdultsChange}
+                />
+            <button
+            id="post-button"
+            type="submit"
+            style={{ width: "60px", height: "100px" }}
+            >Submit
+            </button>
+            </form>
         </div>
     )
 }
