e3cd1bfc627ba3a125f7157998a763333b36c6b5
Daniel <dsalvatori2008@gmail.com>
2024-11-08 16:05:44 +0000

    Admin, facilities, form set up for facilites, scopable

--- brooke_farm_glamping/src/Components/Admin/Facilities/facility.js
+++ brooke_farm_glamping/src/Components/Admin/Facilities/facility.js
@@ -8,6 +8,10 @@ import './facility.css'
 const AdminFacilityComponent = ({facility}) => {
 
     const [fName, setFName] = useState(facility.name)
+    const [fAmount, setFAmount] = useState(facility.amount)
+    const [fImageURL, setFImageURL] = useState(facility.name)
+    const [fMaxPeople, setFMaxPeople] = useState(facility.maxPeople)
+    const [fPrice, setFPrice] = useState('£' + facility.price)
 
     useEffect(() => {
     
@@ -16,14 +20,33 @@ const AdminFacilityComponent = ({facility}) => {
     const handleFNameChange = (evt) => {
         setFName(evt.target.value);
     }
-   
+    const handleFAmountChange = (evt) => {
+        setFAmount(evt.target.value);
+    }
+    const handleFMaxPeopleChange = (evt) => {
+        setFMaxPeople(evt.target.value);
+    }
+    const handleFPriceChange = (evt) => {
+        setFPrice(evt.target.value);
+    }   
 
     return (
         <div className="admin-facilities-edit">
-            <label>{facility.name}</label><br></br>
+            <h3>{facility.name}</h3><br></br>
                 
             <label for="fname">Name:</label><br></br>
             <input type="text" id="fname" name="fname" value={fName} onChange={handleFNameChange}/><br></br>
+            
+            <label for="fAmount">Max facilities available per day:</label><br></br>
+            <input type="text" id="fAmount" name="fAmount" value={fAmount} onChange={handleFAmountChange}/><br></br>
+
+            <label for="fMaxPeople">Max people to stay in the facility:</label><br></br>
+            <input type="text" id="fMaxPeople" name="fMaxPeople" value={fMaxPeople} onChange={handleFMaxPeopleChange}/><br></br>
+
+            <label for="fPrice">Price per night:</label><br></br>
+            <input type="text" id="fPrice" name="fPrice" value={fPrice} onChange={handleFPriceChange}/><br></br>
+
+            {/* Need to work out how to change images */}
 
         </div>
     )
