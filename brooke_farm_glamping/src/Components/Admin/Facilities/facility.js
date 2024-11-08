82be6f9c9a7ce786c79d6897163d0f1dd4bde46c
Daniel <dsalvatori2008@gmail.com>
2024-11-08 15:54:42 +0000

    Admin, facilities, form setting up and name comes through

--- brooke_farm_glamping/src/Components/Admin/Facilities/facility.js
+++ brooke_farm_glamping/src/Components/Admin/Facilities/facility.js
@@ -1,26 +1,31 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import './facility.css'
 
 
 
 
-const AdminFacilityComponent = ({}) => {
+const AdminFacilityComponent = ({facility}) => {
 
-    
+    const [fName, setFName] = useState(facility.name)
 
     useEffect(() => {
-        
     
       }, []);
 
-    
+    const handleFNameChange = (evt) => {
+        setFName(evt.target.value);
+    }
    
 
     return (
-        <>
-            hello
+        <div className="admin-facilities-edit">
+            <label>{facility.name}</label><br></br>
+                
+            <label for="fname">Name:</label><br></br>
+            <input type="text" id="fname" name="fname" value={fName} onChange={handleFNameChange}/><br></br>
 
-        </>
+        </div>
     )
 }
 
